c9d9e7ff67d8661f2c3c6696edee20dce32db5b4
David B. Stocker <coffeemetalcode@gmail.com>
2021-01-19 22:08:24 -0500

    add mongoose, dotenv, some routes

--- app/app.js
+++ app/app.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Post = require('./models/post');
 
 const app = express();
+dotenv.config()
+
+const dbName = process.env.MONGO_DB_NAME;
+const dbUser = process.env.MONGO_DB_USER;
+const dbPwd = process.env.MONGO_DB_PWD;
+
+const dbURI = `mongodb+srv://${dbUser}:${dbPwd}@cluster0.vj608.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(
+  dbURI,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {
+  console.log('database connection initiated');
+}).catch(() => {
+  console.log('databse connection failed');
+});
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,31 +40,41 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log('post from the client', post);
-  res.status(201).json({
-    message: 'new post created'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  console.log('new post: ', post);
+  post.save()
+    .then((result) => {
+      res.status(201).json({
+        message: 'new post created',
+        id: result._id
+      });
+    });
+    
 });
 
 app.get('/api/posts', (req, res, next) => {
-  let posts = 
-  [
-    {
-      id: 'abc123',
-      title: 'Dummy Server Post',
-      content: 'Lorem ipsum, etc.'
-    },
-    {
-      id: 'abc456',
-      title: 'Second Dummy Server Post',
-      content: 'Lorem ipsum, etc.'
-    }
-  ];
-  res.status(200).json({
-    message: 'fecth completed successfully',
-    posts: posts
-  });
+  Post.find()
+    .then((documents) => {
+      res.status(200).json({
+        message: 'fecth completed successfully',
+        posts: documents
+      });
+    });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  Post.deleteOne({_id: id})
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: `post ${id} deleted`
+      })
+    });
 });
 
 module.exports = app;
