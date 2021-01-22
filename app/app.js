8a4af1e04f9534f17315a52cd73f1fc31101e67d
David B. Stocker <coffeemetalcode@gmail.com>
2021-01-22 06:21:12 -0500

    done with section 5

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 dotenv.config()
@@ -39,72 +39,6 @@ app.use((req, res, next) => {
   next();
 });
 
-/* GET posts --> Get all posts */
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then((documents) => {
-      res.status(200).json({
-        message: 'fecth completed successfully',
-        posts: documents
-      });
-    });
-});
-
-/* GET post --> Get one post by id */
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById({ _id: req.params.id })
-    .then((post) => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: 'post not found' });
-      }
-    });
-});
-
-/* POST post --> Add a new post */
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  console.log('new post: ', post);
-  post.save()
-    .then((result) => {
-      res.status(201).json({
-        message: 'new post created',
-        id: result._id
-      });
-    });
-    
-});
-
-/* PUT post --> Update a post by id */
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  Post.updateOne({ _id: req.params.id }, post)
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({
-        message: `post ${req.params.id} has been updated`
-      });
-    });
-});
-
-/* DELETE post --> Delete a post by id */
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id })
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({
-        message: `post ${req.params.id} deleted`
-      });
-    });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
---
+++ app/routes/posts.js
@@ -0,0 +1,75 @@
+const express = require('express');
+
+const Post = require('../models/post');
+
+const router = express.Router();
+
+/* GET posts --> Get all posts */
+router.get('', (req, res, next) => {
+  Post.find()
+    .then((documents) => {
+      res.status(200).json({
+        message: 'fecth completed successfully',
+        posts: documents
+      });
+    });
+});
+
+/* GET post --> Get one post by id */
+router.get('/:id', (req, res, next) => {
+  Post.findById({ _id: req.params.id })
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'post not found' });
+      }
+    });
+});
+
+/* POST post --> Add a new post */
+router.post('', (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  console.log('new post: ', post);
+  post.save()
+    .then((result) => {
+      res.status(201).json({
+        message: 'new post created',
+        id: result._id
+      });
+    });
+    
+});
+
+/* PUT post --> Update a post by id */
+router.put('/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  Post.updateOne({ _id: req.params.id }, post)
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: `post ${req.params.id} has been updated`
+      });
+    });
+});
+
+/* DELETE post --> Delete a post by id */
+router.delete('/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: `post ${req.params.id} deleted`
+      });
+    });
+});
+
+module.exports = router;
